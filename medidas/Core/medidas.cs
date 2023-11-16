e70fd2960e5eb44227fe80879aca31b80b7cc49b
Raven-4 <121634522+Raven-4@users.noreply.github.com>
2023-11-16 11:18:14 +0100

    inicio

---
+++ medidas/Core/medidas.cs
@@ -0,0 +1,14 @@
+class Medidas
+{
+    public double Peso { get; set; }
+    public double CircunferenciaAbdominal { get; set; }
+    public string Notas { get; set; }
+
+    public Medidas(double peso, double circunferenciaAbdominal, string notas)
+    {
+        Peso = peso;
+        CircunferenciaAbdominal = circunferenciaAbdominal;
+        Notas = notas;
+    }
+}
+
