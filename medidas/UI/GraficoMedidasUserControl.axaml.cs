fc01a096065fd4359bcc7c04b8e4394a2785d3ec
susoguim <121634615+susoguim@users.noreply.github.com>
2023-11-22 18:24:14 +0100

    Update GraficoMedidasUserControl.axaml.cs

--- medidas/UI/GraficoMedidasUserControl.axaml.cs
+++ medidas/UI/GraficoMedidasUserControl.axaml.cs
@@ -1,11 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
-using medidas.Core;
 
 namespace medidas.UI;
 
@@ -23,30 +18,6 @@ public partial class GraficoMedidasUserControl : UserControl
     void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
-    }
-
-    private List<Medidas> SacarMedidas(){
-        try
-        {
-            string rutaArchivoXml = "medidas.xml";
-
-            if (File.Exists(rutaArchivoXml))
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(List<Medidas>));
-
-                using (FileStream fileStream = new FileStream(rutaArchivoXml, FileMode.Open))
-                {
-                    return (List<Medidas>)serializer.Deserialize(fileStream);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error al cargar la lista de medidas desde el archivo XML: {ex.Message}");
-        }
-
-        return new List<Medidas>();
-    }
     }
     
     private void CrearGrafico()
