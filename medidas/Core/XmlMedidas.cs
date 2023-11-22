02267b01a7454515fb1b4b4ae8572df0decb1f77
susoguim <121634615+susoguim@users.noreply.github.com>
2023-11-22 18:24:11 +0100

    Update XmlMedidas.cs

--- medidas/Core/XmlMedidas.cs
+++ medidas/Core/XmlMedidas.cs
@@ -30,5 +30,29 @@ public class GuardarMedidas
         }catch (Exception ex){
         }
     }
+
+   private List<Medidas> SacarMedidas(){
+        try
+        {
+            string rutaArchivoXml = "medidas.xml";
+
+            if (File.Exists(rutaArchivoXml))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Medidas>));
+
+                using (FileStream fileStream = new FileStream(rutaArchivoXml, FileMode.Open))
+                {
+                    return (List<Medidas>)serializer.Deserialize(fileStream);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al cargar la lista de medidas desde el archivo XML: {ex.Message}");
+        }
+
+        return new List<Medidas>();
+    }
+    }
 }
 }
