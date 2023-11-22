d3be8e1b3ad450403ab18cf6bd7dc6c5bc060efa
susoguim <121634615+susoguim@users.noreply.github.com>
2023-11-22 17:26:53 +0100

    Update MedidasUserControl.axaml.cs

--- medidas/UI/MedidasUserControl.axaml.cs
+++ medidas/UI/MedidasUserControl.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace medidas.UI;
 
@@ -25,6 +28,22 @@ public partial class MedidasUserControl : UserControl
 
     public void GuardarMedidas()
     {
-        //LO IMPLEMENTA SUSO
+        try{
+            double peso = Convert.ToDouble(pesoTextBox.Text);
+            double circunferencia = Convert.ToDouble(circunferenciaTextBox.Text);
+            string notas = Convert.ToDouble(notasTextBox.Text);
+
+            Medidas medidas = new Medidas(peso, circunferencia, notas);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Medidas));
+
+            using (TextWriter writer = new StreamWriter("medidas.xml")){
+                serializer.Serialize(writer, medidas);
+            }
+
+            //Console.WriteLine("Medidas guardadas correctamente en el archivo medidas.xml.");
+        }catch (Exception ex){
+            //Console.WriteLine($"Error al guardar las medidas: {ex.Message}");
+        }
     }
 }
